07fbe6c67a967f0f813a59c6d15503f305598b31
Arsh Ilahi <arshilahi74@gmail.com>
2024-09-24 15:37:14 +0530

    d

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ const App = () => {
 
   return (
     <div>
-        <h1>Count: {count}</h1>
+        <h1>{count}</h1>
         <button onClick={()=>setCount((prev)=>prev+1)}>Increment</button>
         <button onClick={()=>setCount((prev)=>prev-1)}>Decrement</button>
     </div>
