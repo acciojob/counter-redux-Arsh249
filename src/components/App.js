3f34738de4838757784b9d09a40298059cd4eb61
Arsh Ilahi <arshilahi74@gmail.com>
2024-09-24 15:23:02 +0530

    a

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [count, setCount] = useState(0)
+
   return (
     <div>
-        {/* Do not remove the main div */}
+        <p>Count: {count}</p>
+        <button onClick={()=>setCount((prev)=>prev+1)}>Increment</button>
+        <button onClick={()=>setCount((prev)=>prev-1)}>Decrement</button>
     </div>
   )
 }
